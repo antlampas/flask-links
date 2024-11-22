08250704c00f17f5e9d691086cc1da024ae2b76e
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-11-22 19:15:07 +0100

    -

--- src/links/show_links.py
+++ src/links/show_links.py
@@ -9,4 +9,4 @@ from . import models
 def show_links():
     return render_template('links.html',links=current_app.database.paginate(current_app.database.select(models.Link)),sectionname="Links",next=request.path)
 
-current_menu.submenu(".links").register(text='Link',order=2,external_url=links_blueprint.uerl_prefix+"/links")
+current_menu.submenu(".links").register(text='Link',order=2,external_url=links_blueprint.url_prefix+"/links")
